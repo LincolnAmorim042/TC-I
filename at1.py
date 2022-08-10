24b158b8753845058efcd95db326e49e337708ac
Lincoln Amorim <54185019+lolincoln@users.noreply.github.com>
2022-08-10 16:15:24 -0400

    Add files via upload

---
+++ at1.py
@@ -0,0 +1,39 @@
+pilha = []
+pilha.append('a')
+pilha.append('b')
+pilha.append('c')
+print('pilha')
+print(pilha)
+print('\nElementos removidos da pilha:')
+print(pilha.pop())
+print(pilha.pop())
+print(pilha.pop())
+print('\npilha:')
+print(pilha)
+
+fila = [] 
+fila.append('a')
+fila.append('b')
+fila.append('c')
+print("\nfila")
+print(fila)
+print("\nElementos removidos da fila")
+print(fila.pop(0))
+print(fila.pop(0))
+print(fila.pop(0))
+print("\nfila:")
+print(fila)
+
+pessoasd = {}
+nomesl = ["Juan", "Pablo"]
+pessoasd['nome'] = nomesl
+idadesl = [22, 37]
+pessoasd['idade'] = idadesl
+print("\ndicionario:")
+print(pessoasd)
+
+nomesd = {"Pedro", "Paulo"}
+idadesd = {"37", "22"}
+pessoasl = [nomesd, idadesd]
+print("\nlista:")
+print(pessoasl)
